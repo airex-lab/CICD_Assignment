e5636d1e08262fbbae6b2cd1a1f714983fc2761b
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2024-03-20 12:17:14 +0530

    use logistic regression with a fixed random state

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+model = LogisticRegression(random_state=42).fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
