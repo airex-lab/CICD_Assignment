beb5389b55261b43a501d0a8822ccabc706bbbe8
Shivamiisc <ashivam@iisc.ac.in>
2024-03-29 18:46:56 +0530

    changing the classifier to randomforest

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 import pickle
 import numpy as np
 
@@ -9,7 +9,9 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model =DecisionTreeClassifier(random_state=42).fit(X, y)
+model =RandomForestClassifier(bootstrap=True, random_state=42, max_depth=50, max_features=2,
+                                    min_samples_leaf=5, min_samples_split=8, n_estimators=200).fit(X, y)
+
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
