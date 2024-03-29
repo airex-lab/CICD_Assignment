db665a6c1322a385946cb7abde01544e36e51f35
Shivamiisc <ashivam@iisc.ac.in>
2024-03-29 18:56:08 +0530

    adding error in code

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 import numpy as np
-
+error
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
