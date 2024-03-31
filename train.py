fa78d007f9a4fc703d323914d57795aeb33b0f85
Shreyas <shreg@cisco.com>
2024-03-31 14:04:35 +0530

    Using GNB model for classification In train.py used Gaussian Naive Bayes instead of Logistic Regression

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
 import pickle
 import numpy as np
 
@@ -13,7 +14,8 @@ y = np.array([np.where(labels == x) for x in y]).flatten()
 
 #model = LogisticRegression()
 #model = RandomForestClassifier(n_estimators=100, random_state=42)
-model = SVC(kernel='linear', random_state=42)
+#model = SVC(kernel='linear', random_state=42)
+model = GaussianNB()
 model.fit(X, y)
 
 with open("model.pkl", 'wb') as f:
