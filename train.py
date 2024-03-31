ac9663e95c318bf5c306b93ad095107687d43945
Harsha J K <harshajk41@gmail.com>
2024-03-31 18:43:14 +0530

    Update train.py for accuracy greater than 0.5

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
+from sklearn.naive_bayes import GaussianNB
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -9,7 +10,13 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+get_test_success = 1
+
+if get_test_success == 1:
+    # To get a model that has an accuracy of > 0.5
+    model = GaussianNB().fit(X, y)
+else:
+    model = LogisticRegression().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
