796d0012e92084db874a76f4cfb2c9907bebe483
Shreyas <shreg@cisco.com>
2024-03-31 13:42:54 +0530

    SVM

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import pickle
 import numpy as np
 
@@ -10,9 +11,9 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-#model = LogisticRegression().fit(X, y)
-
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+#model = LogisticRegression()
+#model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = SVC(kernel='linear', random_state=42)
 model.fit(X, y)
 
 with open("model.pkl", 'wb') as f:
