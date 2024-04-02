d640455b7afef6caaedbee6f99fa19c5bc3378e2
sambandh pradhan <35633806+sambandhpradhan@users.noreply.github.com>
2024-04-02 23:47:08 +0530

    Updated train.py to improve model perform

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -9,7 +10,13 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+better_score = 1
+if better_score == 1:
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    model = LogisticRegression(penalty='l2', C=0.001, max_iter=1000).fit(X_scaled, y)
+else:
+    model = LogisticRegression().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
