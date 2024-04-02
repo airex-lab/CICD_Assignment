858bec1289fbe0430226b6fd80daec40c203040e
Anoop Pv <anooppv123@gmail.com>
2024-04-03 02:52:11 +0530

    Adjust train.py for scenario both-actions-fail

--- train.py
+++ train.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 import pickle
 import numpy as np
 
-df = pd.read_csv("data/train.csv")
+df = pd.read_csv("data/train.csv"
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+#model = LogisticRegression().fit(X, y)
+model = GaussianNB().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
